093984403176e53d7bba6c7a3bee68d85d00882a
helga-khols <97860059+helga-khols@users.noreply.github.com>
2023-01-15 17:55:21 +0200

    start

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
-import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import React from 'react';
+import { createRoot} from 'react-dom/client';
+
+// import ReactDOM from 'react-dom';
+
+const elem1 = React.createElement('span', {children: 'Hello'});
+const elem2 = React.createElement('span', {children: 'World'});
+
+const element = React.createElement('div', { a: 5, b: 10, children: [elem1, ' ', elem2]});
+console.log(element);
+
+// ReactDOM.render(element, document.querySelector('#root'));
+
+
+const container = document.querySelector('#root');
+const root = createRoot(container);
+root.render(element);
